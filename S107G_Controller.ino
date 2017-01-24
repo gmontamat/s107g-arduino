8f3f3db0f780ea7f6d51b6ea3b68006802fc664c
Gustavo Montamat <gmontamat@gmail.com>
2017-01-23 21:55:08 -0500

    Minor style fixes

--- S107G_Controller.ino
+++ S107G_Controller.ino
@@ -1,11 +1,11 @@
 /*
-   S107G RC helicopter Arduino controller (2-channel, 32-bit version)
+   Syma S107G RC helicopter Arduino controller (2-channel, 32-bit version)
 
-   Replaces the remote controller of the S107G with commands passed through
-   the serial port by using an array of infrarred LEDs attached to pin 3.
+   Emulates the remote control of the S107G with commands passed through
+   the serial port, using an array of infrarred LEDs attached to pin 3.
 
    The circuit:
-   * Two or three 940nm IR LEDs and a resistor (see http://led.linear1.org/led.wiz)
+   * Two/three 940nm IR LEDs and a resistor (see http://led.linear1.org/led.wiz)
 
    Based on:
    * http://www.jimhung.co.uk/?p=901
@@ -24,7 +24,7 @@ void setup() {
 
   Serial.begin(9600);
 
-  inputBuffer[0] = byte(0);   // channel (0 OR 128)
+  inputBuffer[0] = byte(0);   // channel (0 for A OR 128 for B)
   inputBuffer[1] = byte(63);  // yaw (0 to 127)
   inputBuffer[2] = byte(63);  // pitch (0 to 127)
   inputBuffer[3] = byte(0);   // throttle (0 to 127)
@@ -48,35 +48,32 @@ void sendHeader() {
 
 void sendFooter() {
   sendPulse(312);
+  delayMicroseconds(2001);
 }
 
 void sendControlPacket(byte channel, byte yaw, byte pitch, byte throttle, byte trim_) {
-  byte dataPointer, maskPointer;
-  byte data[4];
   const byte mask[] = {1, 2, 4, 8, 16, 32, 64, 128};
+  byte dataPointer = 0, maskPointer = 8;
+  byte data[4];
 
   data[0] = yaw;
   data[1] = pitch;
-  data[2] = throttle;
+  data[2] = throttle + channel;
   data[3] = trim_;
-  dataPointer = 4;
-  maskPointer = 8;
-
-  data[2] = data[2] + channel;
 
   sendHeader();
 
   // Send 32-bit command
-  while (dataPointer > 0) {
+  while (dataPointer < 4) {
     sendPulse(312);
-    if(data[4 - dataPointer] & mask[--maskPointer]) {
+    if(data[dataPointer] & mask[--maskPointer]) {
       delayMicroseconds(700); // 1
     } else {
       delayMicroseconds(300); // 0
     }
     if (!maskPointer) {
       maskPointer = 8;
-      dataPointer--;
+      dataPointer++;
     }
   }
 
@@ -85,17 +82,18 @@ void sendControlPacket(byte channel, byte yaw, byte pitch, byte throttle, byte t
 
 void loop() {
   static unsigned long millisLast = millis();
-  int cycleTime;
+  int interval;
 
   if (inputBuffer[3] > 0) {
+    // Send command to helicopter
     sendControlPacket(
       inputBuffer[0], inputBuffer[1], inputBuffer[2],
       inputBuffer[3], inputBuffer[4]
     );
   }
 
-  // Print control data to serial monitor
-  Serial.print("Channel = ");
+  // Print command data in serial monitor
+  Serial.print(" Channel = ");
   Serial.print(inputBuffer[0], DEC);
   Serial.print("\tYaw = ");
   Serial.print(inputBuffer[1], DEC);
@@ -106,12 +104,13 @@ void loop() {
   Serial.print("\tTrim = ");
   Serial.println(inputBuffer[4], DEC);
 
-  if (inputBuffer[0] == byte(0)) {
-    cycleTime = 120;
+  // Wait before sending next command
+  if (!inputBuffer[0]) {
+    interval = 120;  // Channel A
   } else {
-    cycleTime = 180;
+    interval = 180;  // Channel B
   }
-  while (millis() < millisLast + cycleTime);
+  while (millis() < millisLast + interval);
   millisLast = millis();
 }
 
